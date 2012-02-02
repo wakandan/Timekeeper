f452b5cbb075535aa08c40054ab79f7cc6054181
Dang Nguyen Anh Khoa <wakandan@gmail.com>
2012-02-03 01:08:02 +0800

    - Made the program resume from previous interruption. The interrupted period will be marked as '...program interrupted'

--- timekeeper.py
+++ timekeeper.py
@@ -9,6 +9,7 @@
 
 from datetime import datetime
 import sys
+import os
 
 ##Copied from: http://code.activestate.com/recipes/134892/
 
@@ -47,19 +48,45 @@ class _GetchWindows:
     def __call__(self):
         import msvcrt
         return msvcrt.getch()
-      
+
+STRFMT_DATETIME = "%a %d/%m/%Y %I:%M%p"
+STRFMT_FILENAME = "%d%m%Y"
+STRFMT_OUTPUT = "%s, %4.2f, "
+
 getch = _Getch()
 
-
+def get_diff_msg(prev, current):
+    '''return the msg of <current_time>, <time_taken>'''
+    
+    difference = (current-prev).total_seconds()*1.0/60
+    current_str = current.strftime(STRFMT_DATETIME)
+    output_str = STRFMT_OUTPUT % (current_str, difference)
+    return output_str
+      
 current = prev = datetime.now()
-f = open(sys.argv[0]+'.'+datetime.now().strftime("%d%m%Y")+'.txt', 'w')
+filename = sys.argv[0]+'.'+datetime.now().strftime(STRFMT_FILENAME)+'.txt'
+
+#if the file existed, open to append
+file_existed = os.path.exists(filename)
+
+if file_existed:
+    #get the last time stamp
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        if len(lines)>0:        
+            prev = datetime.strptime(lines[-1].split(',')[0], STRFMT_DATETIME)
+    #Added msg '...program restarted' to the log file
+    with open(filename, 'a') as f:
+        current = datetime.now()
+        f.write(get_diff_msg(prev, current)+' ...program interrupted\n')
+        prev = current
+            
+f = open(filename, file_existed and 'a' or 'w')
+
 c = getch()
 while c!=chr(27):    
   if c==' ' or c==chr(13):
-    current = datetime.now()
-    current_str = current.strftime("%a %d/%m/%Y %I:%M%p")
-    difference = (current-prev).total_seconds()*1.0/60
-    output_str = "%s, %4.2f, " % (current_str, difference)
+    output_str = get_diff_msg(prev, datetime.now())    
     print output_str,
     reason = raw_input()
     f.write(output_str+reason+'\n')
